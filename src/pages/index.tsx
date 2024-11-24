5a8c5660a77d88f91a5ed3e59f6205c287471f74
Jamie Maclean <jamie.maclean@numberica.com>
2024-11-24 23:31:14 +0100

    remove frontend legacy code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,14 +14,6 @@ type ImplementationType = {
 };
 const implementations: ImplementationType[] = [
   {
-    name: "Parametric Temperature-Time Curves",
-    reference: "EN 1991 Part 1-2 Annex A",
-    tags: ["Fire Scenario", "Temp Vs Time"],
-    svg: FireIcon,
-    colors: "text-red-700 bg-red-50",
-    stateFunction: commands.parametricFireDefaultState,
-  },
-  {
     name: "Heat Content of Plume",
     reference: "PD7974-2:2019 Section 7.1",
     tags: ["Fire Scenario", "Temp Vs Time"],
@@ -44,70 +36,6 @@ const implementations: ImplementationType[] = [
     svg: FireIcon,
     colors: "text-red-700 bg-red-50",
     method: "SFPEAlpertHeatReleaseFromTemperatureAndPosition",
-  },
-  {
-    name: "BR187 Rdiation View Factors",
-    reference: "BR 187 Appendix A",
-    tags: ["Radiation", "Heat Flux"],
-    svg: WifiIcon,
-    colors: "text-yellow-700 bg-yellow-50",
-    stateFunction: commands.br187BasicDefaultState,
-  },
-  {
-    name: "Parametric Temperature-Time Curves",
-    reference: "EN 1991 Part 1-2 Annex A",
-    tags: ["Fire Scenario", "Temp Vs Time"],
-    svg: FireIcon,
-    colors: "text-red-700 bg-red-50",
-    stateFunction: commands.parametricFireDefaultState,
-  },
-  {
-    name: "BR187 Rdiation View Factors",
-    reference: "BR 187 Appendix A",
-    tags: ["Radiation", "Heat Flux"],
-    svg: WifiIcon,
-    colors: "text-yellow-700 bg-yellow-50",
-    stateFunction: commands.br187BasicDefaultState,
-  },
-  {
-    name: "Parametric Temperature-Time Curves",
-    reference: "EN 1991 Part 1-2 Annex A",
-    tags: ["Fire Scenario", "Temp Vs Time"],
-    svg: FireIcon,
-    colors: "text-red-700 bg-red-50",
-    stateFunction: commands.parametricFireDefaultState,
-  },
-  {
-    name: "Parametric Temperature-Time Curves",
-    reference: "EN 1991 Part 1-2 Annex A",
-    tags: ["Fire Scenario", "Temp Vs Time"],
-    svg: FireIcon,
-    colors: "text-red-700 bg-red-50",
-    stateFunction: commands.parametricFireDefaultState,
-  },
-  {
-    name: "Parametric Temperature-Time Curves",
-    reference: "EN 1991 Part 1-2 Annex A",
-    tags: ["Fire Scenario", "Temp Vs Time"],
-    svg: FireIcon,
-    colors: "text-red-700 bg-red-50",
-    stateFunction: commands.parametricFireDefaultState,
-  },
-  {
-    name: "Parametric Temperature-Time Curves",
-    reference: "EN 1991 Part 1-2 Annex A",
-    tags: ["Fire Scenario", "Temp Vs Time"],
-    svg: FireIcon,
-    colors: "text-red-700 bg-red-50",
-    stateFunction: commands.parametricFireDefaultState,
-  },
-  {
-    name: "Parametric Temperature-Time Curves",
-    reference: "EN 1991 Part 1-2 Annex A",
-    tags: ["Fire Scenario", "Temp Vs Time"],
-    svg: FireIcon,
-    colors: "text-red-700 bg-red-50",
-    stateFunction: commands.parametricFireDefaultState,
   },
 ];
 
@@ -194,100 +122,6 @@ function IndexPage() {
         </div>
       </form>
 
-      <div className="flex flex-col flex-1 w-full">
-        <div className="flex pt-4 sm:pt-6 h-full">
-          <h1 className="text-2xl font-semibold leading-7 text-gray-900 flex flex-row">
-            Builder
-          </h1>
-        </div>
-        <div className="flex items-center justify-between py-0">
-          <h1 className="text-xl font-semibold leading-7 text-gray-500">
-            Create custom calculations and methods
-          </h1>
-        </div>
-      </div>
-
-      <button
-        className="bg-white hover:bg-gray-100 w-full"
-        onClick={() => commands.newMethodBuilder()}
-      >
-        <div className="flex w-full space-x-6 p-6 items-center">
-          <div className="flex-1">
-            <div className="flex flex-col items-start">
-              <h3 className="text-lg font-medium text-gray-900">
-                Method Builder
-              </h3>
-            </div>
-          </div>
-          <WrenchScrewdriverIcon
-            className={`h-10 w-10 flex-shrink-0 rounded-full p-1 red-700 bg-red-50`}
-          />
-        </div>
-      </button>
-
-      <div className="flex flex-col flex-1 w-full">
-        <div className="flex pt-4 sm:pt-6 h-full">
-          <h1 className="text-2xl font-semibold leading-7 text-gray-900 flex flex-row">
-            Flows
-          </h1>
-        </div>
-        <div className="flex items-center justify-between py-0">
-          <h1 className="text-xl font-semibold leading-7 text-gray-500">
-            Calculation flows that combine multiple methods
-          </h1>
-        </div>
-      </div>
-      <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-10">
-        {implementations.map((implementation: ImplementationType, index) => {
-          const Icon = implementation.svg;
-          return (
-            <li
-              key={index}
-              className="col-span-1 divide-y divide-gray-200 rounded-lg shadow"
-            >
-              <button
-                className="bg-white hover:bg-gray-100 w-full"
-                onClick={() => {
-                  if (implementation.method) {
-                    commands.setCurrentTabMethod(implementation.method);
-                  } else if (implementation.stateFunction) {
-                    implementation.stateFunction().then((state) => {
-                      state.status == "ok" &&
-                        updateTab(currentTab?.id, state.data);
-                    });
-                  }
-                }}
-              >
-                <div className="flex w-full space-x-6 p-6">
-                  <div className="flex-1">
-                    <div className="flex flex-col items-start mb-2">
-                      <h3 className="text-sm font-medium text-gray-900">
-                        {implementation.name}
-                      </h3>
-                      <p className="mt-1 text-sm text-gray-500">
-                        {implementation.reference}
-                      </p>
-                    </div>
-                    <div className="flex gap-1">
-                      {implementation.tags.map((tag) => (
-                        <span
-                          key={tag}
-                          className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
-                        >
-                          {tag}
-                        </span>
-                      ))}
-                    </div>
-                  </div>
-                  <Icon
-                    className={`h-10 w-10 flex-shrink-0 rounded-full p-1 ${implementation.colors}`}
-                  />
-                </div>
-              </button>
-            </li>
-          );
-        })}
-      </ul>
       <div className="flex flex-col flex-1 w-full">
         <div className="flex pt-4 sm:pt-6 h-full">
           <h1 className="text-2xl font-semibold leading-7 text-gray-900 flex flex-row">
--- src/routes/explore/index.tsx
+++ src/routes/explore/index.tsx
@@ -1,6 +1,5 @@
 import "katex/dist/katex.min.css";
 import { useEffect } from "react";
-// import { TABS, TabNames } from "./constants";
 import TabBar from "./Tabs";
 import {
   register,
@@ -8,14 +7,8 @@ import {
   isRegistered,
 } from "@tauri-apps/plugin-global-shortcut";
 import { useTabs } from "../../tabs/tabProvider";
-import Calculation from "src/pages/eurocode_1_1_2/calculation";
-import Br187Basic from "src/pages/br187/annexA/calculation";
-import Br187BasicResults from "src/pages/br187/annexA/results";
-import Results from "src/pages/eurocode_1_1_2/results";
-import ParametricFireAbout from "src/pages/eurocode_1_1_2/about";
-import PlumeHeatContent from "src/pages/Method";
+import Method from "src/pages/Method";
 import IndexPage from "src/pages";
-import MethodBuilder from "src/pages/MethodBuilder";
 
 export default function Explore() {
   const { currentTab, newTab } = useTabs();
@@ -40,27 +33,7 @@ export default function Explore() {
       <div className="mt-10 pb-10 flex items-center flex-col overflow-y-auto h-full">
         {currentTab === null && <div>Loading...</div>}
         {currentTab?.state.type == "Index" && <IndexPage />}
-        {currentTab?.state.type == "ParametricFire" && (
-          <Calculation tab={currentTab} />
-        )}
-        {currentTab?.state.type == "Br187Basic" && (
-          <Br187Basic tab={currentTab} />
-        )}
-        {currentTab?.state.type == "ParametricFireResults" && (
-          <Results tab={currentTab} />
-        )}
-        {currentTab?.state.type == "ParametricFireAbout" && (
-          <ParametricFireAbout />
-        )}
-        {currentTab?.state.type == "Br187BasicResults" && (
-          <Br187BasicResults tab={currentTab} />
-        )}
-        {currentTab?.state.type == "Method" && (
-          <PlumeHeatContent tab={currentTab} />
-        )}
-        {currentTab?.state.type == "MethodBuilder" && (
-          <MethodBuilder tab={currentTab} />
-        )}
+        {currentTab?.state.type == "Method" && <Method tab={currentTab} />}
       </div>
     </div>
   );
