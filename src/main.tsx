1f5ccdfb621c9fa5405974de479fc1258b8be152
Jamie Maclean <jamie.maclean@numberica.com>
2024-11-28 22:39:55 +0100

    fix async issue in update script

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./routes/Root";
 import "./styles.css";
@@ -7,35 +7,6 @@ import { TabProvider } from "src/tabs/tabProvider";
 import ReactGA from "react-ga4";
 import { check } from "@tauri-apps/plugin-updater";
 import { relaunch } from "@tauri-apps/plugin-process";
-
-if (import.meta.env.ENVIRONMENT === "release") {
-  const update = await check();
-  if (update) {
-    console.log(
-      `found update ${update.version} from ${update.date} with notes ${update.body}`
-    );
-    let downloaded = 0;
-    let contentLength: number | undefined = 0;
-    await update.downloadAndInstall((event) => {
-      switch (event.event) {
-        case "Started":
-          contentLength = event.data.contentLength;
-          console.log(`started downloading ${event.data.contentLength} bytes`);
-          break;
-        case "Progress":
-          downloaded += event.data.chunkLength;
-          console.log(`downloaded ${downloaded} from ${contentLength}`);
-          break;
-        case "Finished":
-          console.log("download finished");
-          alert("Download finished, please restart the app");
-          break;
-      }
-    });
-
-    await relaunch();
-  }
-}
 
 ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID);
 
@@ -48,6 +19,43 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  useEffect(() => {
+    const checkForUpdate = async () => {
+      if (import.meta.env.ENVIRONMENT === "release") {
+        const update = await check();
+        if (update) {
+          console.log(
+            `found update ${update.version} from ${update.date} with notes ${update.body}`
+          );
+          let downloaded = 0;
+          let contentLength: number | undefined = 0;
+          await update.downloadAndInstall((event) => {
+            switch (event.event) {
+              case "Started":
+                contentLength = event.data.contentLength;
+                console.log(
+                  `started downloading ${event.data.contentLength} bytes`
+                );
+                break;
+              case "Progress":
+                downloaded += event.data.chunkLength;
+                console.log(`downloaded ${downloaded} from ${contentLength}`);
+                break;
+              case "Finished":
+                console.log("download finished");
+                alert("Download finished, please restart the app");
+                break;
+            }
+          });
+
+          await relaunch();
+        }
+      }
+    };
+
+    checkForUpdate();
+  }, []);
+
   return (
     <TabProvider>
       <RouterProvider router={router} />
