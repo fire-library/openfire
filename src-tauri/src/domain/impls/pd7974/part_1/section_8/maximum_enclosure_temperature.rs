33d74d383fb75ace0f765ef35d0ff8673492d650
Jamie Maclean <jamie.maclean@numberica.com>
2024-12-08 23:08:58 +0100

    do full calc

--- src-tauri/src/domain/impls/pd7974/part_1/section_8/maximum_enclosure_temperature.rs
+++ src-tauri/src/domain/impls/pd7974/part_1/section_8/maximum_enclosure_temperature.rs
@@ -9,7 +9,7 @@ use crate::domain::method::parameter::Parameters;
 use crate::domain::method::parameter::{ParameterTrait, ParametersTrait};
 use crate::domain::method::MethodType;
 use crate::domain::method::{step::Step, Method};
-use pd_7974::part_1::section_8::{equation_41, equation_42};
+use pd_7974::part_1::section_8::{equation_41, equation_42, equation_43, equation_44};
 use std::sync::{Arc, RwLock};
 
 pub struct MaximumEnclosureTemperatureBuilder;
@@ -51,8 +51,32 @@ impl MethodBuilderTrait for MaximumEnclosureTemperatureBuilder {
             .required()
             .build();
 
-        let t_g = ParameterBuilder::float("T_{g(max)}")
+        let m_e = ParameterBuilder::float("m_e")
+            .name("Equivalent fire load as wood")
+            .units("kg")
+            .min_exclusive(0.0)
+            .build();
+
+        let psi = ParameterBuilder::float("\\Psi")
+            .name("Non dimensional input Psi")
+            .build();
+
+        let omega = ParameterBuilder::float("\\Omega")
+            .name("Non dimensional input Omega")
+            .build();
+
+        let t_g_max = ParameterBuilder::float("T_{g(max)}")
             .name("Maximum enclosure temperature")
+            .expression(Box::new(MaximumEnclosureTemperature::new(
+                a_t.clone(),
+                a_v.clone(),
+                h_v.clone(),
+            )))
+            .units("^{o}C")
+            .build();
+
+        let t_g = ParameterBuilder::float("T_{g}")
+            .name("Average enclosure temperature")
             .expression(Box::new(MaximumEnclosureTemperature::new(
                 a_t.clone(),
                 a_v.clone(),
@@ -64,26 +88,44 @@ impl MethodBuilderTrait for MaximumEnclosureTemperatureBuilder {
         params.add(a_t);
         params.add(a_v);
         params.add(h_v);
+        params.add(m_e);
+        params.add(psi);
+        params.add(omega);
+        params.add(t_g_max);
         params.add(t_g);
 
         return params;
     }
     fn form(params: &Parameters) -> crate::domain::method::form::Form {
-        let mut fields = vec![];
+        let mut fields_step_1 = vec![];
+        let mut fields_step_2 = vec![];
         for param in params.values().into_iter() {
-            if param.read().unwrap().id == "T_{g(max)}" {
+            if param.read().unwrap().id == "T_{g(max)}"
+                || param.read().unwrap().id == "T_{g}"
+                || param.read().unwrap().id == "\\Psi"
+                || param.read().unwrap().id == "m_e"
+                || param.read().unwrap().id == "\\Omega"
+            {
                 continue;
             }
-            fields.push(param.to_field())
+            fields_step_1.push(param.to_field())
         }
+        fields_step_2.push(params.get_parameter("m_e").to_field());
         let step_1 = FormStep {
             name: "Ceiling Jet Correlation Input".to_string(),
             description: "Input required to calculate the heat release rate".to_string(),
-            fields: fields,
+            fields: fields_step_1,
+        };
+        let step_2 = FormStep {
+            name: "Low fire load input (optional)".to_string(),
+            description:
+                "Calculate the impact of low fire load on the average temperature in the compartment"
+                    .to_string(),
+            fields: fields_step_2,
         };
 
         Form {
-            steps: vec![step_1],
+            steps: vec![step_1, step_2],
         }
     }
 
@@ -108,13 +150,26 @@ pub fn evaluate(method: &mut Method) -> Result<(), String> {
     let a_t = method.parameters.get_parameter("A_t").as_float();
     let a_v = method.parameters.get_parameter("A_v").as_float();
     let h_v = method.parameters.get_parameter("H_v").as_float();
+    let m_e = method.parameters.get_parameter("m_e");
 
-    let temp = method.parameters.get_parameter("T_{g(max)}");
+    let omega = method.parameters.get_parameter("\\Omega");
+    let psi = method.parameters.get_parameter("\\Psi");
+    let t_max = method.parameters.get_parameter("T_{g(max)}");
+    let t_g = method.parameters.get_parameter("T_{g}");
 
-    let omega = equation_42::calculate(a_t, a_v, h_v);
-    let t = equation_41::calculate(omega);
+    let omega_result = equation_42::calculate(a_t, a_v, h_v);
+    omega.write().unwrap().value = Some(ParameterValue::Float(omega_result));
 
-    temp.write().unwrap().value = Some(ParameterValue::Float(t));
+    let t = equation_41::calculate(omega_result);
+    t_max.write().unwrap().value = Some(ParameterValue::Float(t));
+
+    if let Some(ParameterValue::Float(m_e)) = m_e.read().unwrap().value {
+        let psi_result = equation_44::calculate(m_e, a_v, a_t);
+        psi.write().unwrap().value = Some(ParameterValue::Float(psi_result));
+
+        let t_g_result = equation_43::calculate(t, psi_result);
+        t_g.write().unwrap().value = Some(ParameterValue::Float(t_g_result));
+    }
 
     return Ok(());
 }
