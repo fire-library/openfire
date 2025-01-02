69ad0eca81f71462e0e0fa461047a049f5044dfe
Jamie Maclean <jamie.maclean@numberica.com>
2025-01-02 23:45:19 +0000

    create manual update flow

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { TabProvider } from "src/tabs/tabProvider";
 import { UserAgreementProvider } from "src/tabs/userAgreementProvider";
+import { UpdateProvider } from "src/tabs/useUpdate";
 import ReactGA from "react-ga4";
 import { check } from "@tauri-apps/plugin-updater";
 import { relaunch } from "@tauri-apps/plugin-process";
@@ -20,42 +21,13 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  useEffect(() => {
-    const checkForUpdate = async () => {
-      const update = await check();
-      if (update) {
-        let downloaded = 0;
-        let contentLength: number | undefined = 0;
-        await update.downloadAndInstall((event) => {
-          switch (event.event) {
-            case "Started":
-              contentLength = event.data.contentLength;
-              console.log(
-                `started downloading ${event.data.contentLength} bytes`
-              );
-              break;
-            case "Progress":
-              downloaded += event.data.chunkLength;
-              console.log(`downloaded ${downloaded} from ${contentLength}`);
-              break;
-            case "Finished":
-              console.log("download finished");
-              alert("Download finished, please restart the app");
-              break;
-          }
-        });
-        // await relaunch();
-      }
-    };
-
-    checkForUpdate();
-  }, []);
-
   return (
     <UserAgreementProvider>
-      <TabProvider>
-        <RouterProvider router={router} />
-      </TabProvider>
+      <UpdateProvider>
+        <TabProvider>
+          <RouterProvider router={router} />
+        </TabProvider>
+      </UpdateProvider>
     </UserAgreementProvider>
   );
 }
--- src/tabs/useUpdate.tsx
+++ src/tabs/useUpdate.tsx
@@ -1,29 +1,85 @@
-import { check } from "@tauri-apps/plugin-updater";
+import { check, Update } from "@tauri-apps/plugin-updater";
 import React, {
   ReactNode,
   createContext,
   useState,
   useContext,
-  useEffect,
   useCallback,
 } from "react";
-import { commands } from "src/bindings";
 
 export type UpdateType = {
   checkForUpdate: () => void;
+  progress: number;
+  installing: boolean;
+  awaitingRestart: boolean;
+  update: Update | null;
+  doUpdate: () => void;
 };
 
 const UpdateContext = createContext<UpdateType>({
   checkForUpdate: () => {},
+  doUpdate: () => {},
+  progress: 0,
+  installing: false,
+  awaitingRestart: false,
+  update: null,
 });
 
 export const UpdateProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const checkForUpdate = async () => {};
+  const [update, setUpdate] = useState<Update | null>(null);
+  const [awaitingRestart, setAwaitingRestart] = useState<boolean>(false);
+  const [installing, setInstalling] = useState<boolean>(false);
+  const [progress, setProgress] = useState<number>(0);
+
+  const doUpdate = useCallback(async () => {
+    if (!update) {
+      return;
+    }
+
+    let size = 0;
+    let stateProgress = 0;
+    let localProgress = 0;
+    let downloaded = 0;
+    await update.download((event) => {
+      switch (event.event) {
+        case "Started":
+          size = event.data.contentLength as number;
+          setProgress(0);
+          setInstalling(true);
+          break;
+        case "Progress":
+          downloaded += event.data.chunkLength;
+          localProgress = (downloaded / size) * 100;
+
+          if (localProgress - stateProgress > 5) {
+            stateProgress = localProgress;
+            setProgress(localProgress);
+          }
+          break;
+        case "Finished":
+          setInstalling(false);
+          setAwaitingRestart(true);
+          break;
+      }
+    });
+  }, [update]);
+
+  const checkForUpdate = useCallback(async () => {
+    const update = await check();
+    if (update) {
+      setUpdate(update);
+    }
+  }, []);
 
   const value = {
     checkForUpdate,
+    progress,
+    installing,
+    awaitingRestart,
+    update,
+    doUpdate,
   };
 
   return (
@@ -31,6 +87,6 @@ export const UpdateProvider: React.FC<{ children: ReactNode }> = ({
   );
 };
 
-export function useUserAgreement() {
+export function useUpdate() {
   return useContext(UpdateContext);
 }
