4285af07c2efb1193332bdd813bcbb3568ec3f13
Jamie Maclean <jamie.maclean@numberica.com>
2025-01-03 23:11:48 +0000

    revert debugging changes

--- src/tabs/useUpdate.tsx
+++ src/tabs/useUpdate.tsx
@@ -42,7 +42,7 @@ export const UpdateProvider: React.FC<{ children: ReactNode }> = ({
     let stateProgress = 0;
     let localProgress = 0;
     let downloaded = 0;
-    await update.download((event) => {
+    await update.downloadAndInstall((event) => {
       switch (event.event) {
         case "Started":
           size = event.data.contentLength as number;
