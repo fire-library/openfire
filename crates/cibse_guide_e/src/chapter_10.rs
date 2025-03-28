4043e944fdb93d318806800057ab8e9f69e6703b
simonsantama <simonsantama@gmail.com>
2025-03-28 18:20:14 +0000

    Adds 10.7 to chapter_10.rs2

--- crates/cibse_guide_e/src/chapter_10.rs
+++ crates/cibse_guide_e/src/chapter_10.rs
@@ -1,6 +1,7 @@
 pub mod equation_10_1;
 pub mod equation_10_2;
 pub mod equation_10_3;
+pub mod equation_10_7;
 
 use framework::serde::{Deserialize, Serialize};
 use framework::specta::Type;
@@ -10,6 +11,7 @@ pub enum Chapter10Method {
     Equation10_1,
     Equation10_2,
     Equation10_3,
+    Equation10_7,
 }
 
 impl Chapter10Method {
@@ -18,6 +20,7 @@ impl Chapter10Method {
             &Chapter10Method::Equation10_1 => "equation_10_1".to_string(),
             &Chapter10Method::Equation10_2 => "equation_10_2".to_string(),
             &Chapter10Method::Equation10_3 => "equation_10_3".to_string(),
+            &Chapter10Method::Equation10_7 => "equation_10_7".to_string(),
         }
     }
 
@@ -31,6 +34,9 @@ impl Chapter10Method {
             }
             &Chapter10Method::Equation10_3 => {
                 format!("Ch. 10 | Eq. 10.3")
+            }
+            &Chapter10Method::Equation10_7 => {
+                format!("Ch. 10 | Eq. 10.7")
             }
         }
     }
@@ -46,6 +52,9 @@ impl Chapter10Method {
             &Chapter10Method::Equation10_3 => {
                 include_str!("../resources/chapter_10/equation_10_3/description.md").to_string()
             }
+            &Chapter10Method::Equation10_7 => {
+                include_str!("../resources/chapter_10/equation_10_7/description.md").to_string()
+            }
         }
     }
 
@@ -59,6 +68,9 @@ impl Chapter10Method {
             }
             &Chapter10Method::Equation10_3 => {
                 include_str!("../resources/chapter_10/equation_10_3/limitations.md").to_string()
+            }
+            &Chapter10Method::Equation10_7 => {
+                include_str!("../resources/chapter_10/equation_10_7/limitations.md").to_string()
             }
         }
     }
