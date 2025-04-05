847846e681be9902d40334af3461d7f8c5463214
simonsantama <simonsantama@gmail.com>
2025-04-05 11:52:09 +0100

    Adds equation with symbols to the implementation of 10.12

--- crates/cibse_guide_e/src/chapter_10/equation_10_12.rs
+++ crates/cibse_guide_e/src/chapter_10/equation_10_12.rs
@@ -4,6 +4,13 @@ pub fn limiting_velocity(k: f64, g: f64, q: f64, w: f64, rho: f64, c: f64, t: f6
     return k * ((g*q) / (w * rho * c * t)).powf(1.0/3.0);
 }
 
+fn limiting_velocity_symbols(v_e: String, k: String, g: String, q: String, w: String, rho: String, c: String, t: String) -> String {
+    format!(
+        "{} = {} \\space \\frac{{{} \\space {}}}{{{} \\space {} \\space {} \\space {}}} ^ {{1/3}}",
+        v_e, k, g, q, w, rho, c, t,
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
