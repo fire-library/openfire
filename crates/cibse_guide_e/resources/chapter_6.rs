27b91111552532dd18e8175f090030ae45cbf09c
simonsantama <simonsantama@gmail.com>
2025-04-10 20:39:56 +0100

    Adds method to enums

--- crates/cibse_guide_e/resources/chapter_6.rs
+++ crates/cibse_guide_e/resources/chapter_6.rs
@@ -1,4 +1,5 @@
 pub mod equation_6_7;
+pub mod equation_6_appendix;
 
 use openfire::serde::{Deserialize, Serialize};
 use openfire::specta::Type;
@@ -6,19 +7,26 @@ use openfire::specta::Type;
 #[derive(Clone, Type, Serialize, Deserialize, Debug)]
 pub enum Chapter10Method {
     Equation6_7,
+    Equation6_Appendix,
 }
 
 impl Chapter10Method {
     pub fn id(&self) -> String {
         match self {
-            &Chapter10Method::Equation10_1 => "equation_6_7".to_string(),
+            &Chapter6Method::Equation6_7 => "equation_6_7".to_string(),
+        }
+        match self {
+            &Chapter6Method::Equation6_Appendix => "equation_6_appendix".to_string(),
         }
     }
 
     pub fn friendly_reference(&self) -> String {
         match self {
-            &Chapter10Method::Equation10_1 => {
+            &Chapter6Method::Equation6_7 => {
                 format!("Ch. 6 | Eq. 6.7")
+            }
+            &Chapter6Method::Equation6_Appendix => {
+                format!("Ch. 6 | Appendix")
             }
         }
     }
@@ -28,6 +36,9 @@ impl Chapter10Method {
             &Chapter6Method::Equation6_7 => {
                 include_str!("../resources/chapter_6/equation_6_7/description.md").to_string()
             }
+            &Chapter6Method::Equation6_Appendix => {
+                include_str!("../resources/chapter_6/equation_6_appendix/description.md").to_string()
+            }
         }
     }
 
@@ -35,6 +46,9 @@ impl Chapter10Method {
         match self {
             &Chapter6Method::Equation6_7 => {
                 include_str!("../resources/chapter_6/equation_6_7/limitations.md").to_string()
+            }
+            &Chapter6Method::Equation6_Appendix => {
+                include_str!("../resources/chapter_6/equation_6_appendix/limitations.md").to_string()
             }
         }
     }
