5fcfa7117c043c072b45851864df84d37a798f5b
simonsantama <simonsantama@gmail.com>
2025-05-26 06:10:35 +0100

    Adds equation logic to 6-58

--- crates/cibse_guide_e/src/chapter_6/equation_6_58.rs
+++ crates/cibse_guide_e/src/chapter_6/equation_6_58.rs
@@ -1,3 +1,22 @@
 pub mod openfire_runner;
 
-// Add your equation 6.58 logic here
+pub fn ventcontrolled_rate_of_burning(a_t: f64, a_o: f64, h_o: f64, w: f64, d: f64) -> f64 {
+    return 0.02 * ((a_t - a_o)*(a_o * h_o.powf(0.5))*(w / d)).powf(0.5);
+}
+
+pub fn equation(r: String, a_t: String, a_o: String, h_o: String, w: String, d: String) -> String {
+    format!("{} = 0.02 [({} - {}) \\cdot ({} \\sqrt {{{}}}) \\cdot ({} / {})]^{{1/2}}", r, a_t, a_o, a_o, h_o, w, d)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let result = ventcontrolled_rate_of_burning(45.0, 2.1, 2.1, 3.0, 4.0);
+        assert_eq!(result, 0.197903622836789);
+    }
+}
+
+
