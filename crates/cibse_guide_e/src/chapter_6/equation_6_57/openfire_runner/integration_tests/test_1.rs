8d4cb08dd3dc883b0dca5d39329691c5aa98d62a
simonsantama <simonsantama@gmail.com>
2025-05-26 09:09:15 +0100

    Adds runner and integration test to 6.57

---
+++ crates/cibse_guide_e/src/chapter_6/equation_6_57/openfire_runner/integration_tests/test_1.rs
@@ -0,0 +1,45 @@
+use framework::filesystem::saved_method::{SavedMethod, SavedParameter};
+use framework::method::parameter::ParameterValue::Float;
+use framework::method::runner::MethodRunner;
+use framework::method::test::{Assertion, Test};
+
+pub fn test() -> Test {
+    Test {
+        name: "Test 1".to_string(),
+        description: include_str!("test_1/description.md").to_string(),
+        input: SavedMethod {
+            id: super::super::Chapter6Equation57Runner.id(),
+            parameters: vec![
+                SavedParameter {
+                    name: "R".to_string(),
+                    value: Some(Float(0.2)),
+                },
+                SavedParameter {
+                    name: "w".to_string(),
+                    value: Some(Float(1.0)),
+                },
+                SavedParameter {
+                    name: "h_{o}".to_string(),
+                    value: Some(Float(2.1)),
+                },
+            ],
+        },
+        assertions: vec![Assertion::FloatEqual("z_{fo}".to_string(), 2.27753842349235)],
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        framework::register_runner::<super::super::super::Chapter6Equation57Runner>();
+        let test = test();
+        let results = framework::method::test::run_test(test).unwrap();
+
+        for r in results {
+            assert!(r.has_passed());
+        }
+    }
+}
