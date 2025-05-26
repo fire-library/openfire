2fdcec9177053cb9b5d4c744f6d8fa0e926704a4
simonsantama <simonsantama@gmail.com>
2025-05-26 05:56:37 +0100

    Adds 6-58 to chapter 6.rs

--- crates/cibse_guide_e/src/chapter_6.rs
+++ crates/cibse_guide_e/src/chapter_6.rs
@@ -1,4 +1,5 @@
 pub mod equation_6_7;
+pub mod equation_6_58;
 
 use framework::serde::{Deserialize, Serialize};
 use framework::specta::Type;
@@ -6,35 +7,42 @@ use framework::specta::Type;
 #[derive(Clone, Type, Serialize, Deserialize, Debug)]
 pub enum Chapter6Method {
     Equation6_7,
+    Equation6_58,
 }
 
 impl Chapter6Method {
     pub fn id(&self) -> String {
         match self {
-            &Chapter6Method::Equation6_7 => "equation_6_7".to_string(),
+            Chapter6Method::Equation6_7 => "equation_6_7".to_string(),
+            Chapter6Method::Equation6_58 => "equation_6_58".to_string(),
         }
     }
 
     pub fn friendly_reference(&self) -> String {
         match self {
-            &Chapter6Method::Equation6_7 => {
-                format!("Ch. 6 | Eq. 6.7")
-            }
+            Chapter6Method::Equation6_7 => format!("Ch. 6 | Eq. 6.7"),
+            Chapter6Method::Equation6_58 => format!("Ch. 6 | Eq. 6.58"),
         }
     }
 
     pub fn about_method(&self) -> String {
         match self {
-            &Chapter6Method::Equation6_7 => {
+            Chapter6Method::Equation6_7 => {
                 include_str!("../resources/chapter_6/equation_6_7/description.md").to_string()
+            }
+            Chapter6Method::Equation6_58 => {
+                include_str!("../resources/chapter_6/equation_6_58/description.md").to_string()
             }
         }
     }
 
     pub fn method_limitations(&self) -> String {
         match self {
-            &Chapter6Method::Equation6_7 => {
+            Chapter6Method::Equation6_7 => {
                 include_str!("../resources/chapter_6/equation_6_7/limitations.md").to_string()
+            }
+            Chapter6Method::Equation6_58 => {
+                include_str!("../resources/chapter_6/equation_6_58/limitations.md").to_string()
             }
         }
     }
---
+++ crates/cibse_guide_e/src/chapter_6/equation_6_58.rs
@@ -0,0 +1,3 @@
+pub mod openfire_runner;
+
+// Add your equation 6.58 logic here
