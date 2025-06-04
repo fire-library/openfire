9c855dc48fe24592a929b1a0f739ab07674da05b
simonsantama <simonsantama@gmail.com>
2025-06-04 20:29:43 +0100

    Adds 7.3 implementation of the equation

---
+++ crates/cibse_guide_e/src/chapter_7/equation_7_3.rs
@@ -0,0 +1,23 @@
+pub mod openfire_runner;
+
+pub fn required_width_stair(p: i32, n: i32) -> f64 {
+    return (p as f64 + 15.0 * n as f64 - 15.0) / (150.0 + 50.0 * n as f64);
+}
+
+pub fn equation(w: String, p: String, n: String) -> String {
+    format!(
+        "{} = \\frac{{{} + 15 \\cdot {} - 15}}{{150 + 50 \\cdot {}}}",
+        w, p, n, n
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let result = required_width_stair(550, 6);
+        assert_eq!(result, 1.38888888888889);        
+    }
+}
