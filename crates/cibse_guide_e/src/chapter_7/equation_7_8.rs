4993ed7446cb818fda9021474efdd75af8a57eb6
simonsantama <simonsantama@gmail.com>
2025-06-16 13:14:32 +0100

    Implements verification test for 7.8

--- crates/cibse_guide_e/src/chapter_7/equation_7_8.rs
+++ crates/cibse_guide_e/src/chapter_7/equation_7_8.rs
@@ -2,12 +2,15 @@ pub mod openfire_runner;
 
 pub fn exit_capacity_stair(w_s: f64, t: f64, a: f64, s: i32) -> i32 {
     let s = s as f64;
-    let result = 1.333*w_s * t + 3.5 * a * (s - 1.0);
+    let result = 1.333 * w_s * t + 3.5 * a * (s - 1.0);
     return result.floor() as i32;
 }
 
 pub fn equation(n_in: String, w_s: String, t: String, a: String, s: String) -> String {
-    format!("{} = 1.333 \\cdot {} \\cdot {} + 3.5 \\cdot {} \\cdot ({} - 1)", n_in, w_s, t, a, s)
+    format!(
+        "{} = 1.333 \\cdot {} \\cdot {} + 3.5 \\cdot {} \\cdot ({} - 1)",
+        n_in, w_s, t, a, s
+    )
 }
 
 #[cfg(test)]
---
+++ crates/cibse_guide_e/src/chapter_7/equation_7_8/openfire_runner/integration_tests/test_1.rs
@@ -0,0 +1,50 @@
+use framework::filesystem::saved_method::{SavedMethod, SavedParameter};
+use framework::method::parameter::ParameterValue::Float;
+use framework::method::parameter::ParameterValue::Integer;
+use framework::method::runner::MethodRunner;
+use framework::method::test::{Assertion, Test};
+
+pub fn test() -> Test {
+    Test {
+        name: "Test 1".to_string(),
+        description: include_str!("test_1/description.md").to_string(),
+        input: SavedMethod {
+            id: super::super::Chapter7Equation8Runner.id(),
+            parameters: vec![
+                SavedParameter {
+                    name: "W_s".to_string(),
+                    value: Some(Float(1.2)),
+                },
+                SavedParameter {
+                    name: "t".to_string(),
+                    value: Some(Float(150.0)),
+                },
+                SavedParameter {
+                    name: "A".to_string(),
+                    value: Some(Float(10)),
+                },
+                SavedParameter {
+                    name: "S".to_string(),
+                    value: Some(Integer(5)),
+                },
+            ],
+        },
+        assertions: vec![Assertion::IntegerEqual("N_{in_{max}}".to_string(), 379)],
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        framework::register_runner::<super::super::super::Chapter7Equation8Runner>();
+        let test = test();
+        let results = framework::method::test::run_test(test).unwrap();
+
+        for r in results {
+            assert!(r.has_passed());
+        }
+    }
+}
