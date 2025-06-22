e7ea4e0e03b12acf79a0b05423c90230ec7e1469
simonsantama <simonsantama@gmail.com>
2025-06-22 11:17:22 +0100

    Fixes issue with parameter name

--- crates/cibse_guide_e/src/chapter_7/equation_7_9.rs
+++ crates/cibse_guide_e/src/chapter_7/equation_7_9.rs
@@ -1,8 +1,8 @@
 pub mod openfire_runner;
 
-pub fn acceptance_capacity_stair(w_e: f64, t: f64, p: f64, a: f64, s: i32) -> i32 {
+pub fn acceptance_capacity_stair(w_e: f64, t: f64, rho: f64, a: f64, s: i32) -> i32 {
     let s = s as f64;
-    let result = 1.2 * w_e * t + p * a * (s - 1.0);
+    let result = 1.2 * w_e * t + rho * a * (s - 1.0);
     return result.floor() as i32;
 }
 
