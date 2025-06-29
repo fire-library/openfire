6258bbd9a5625eb1212582b3bc2777ac0ec853d3
simonsantama <simonsantama@gmail.com>
2025-06-29 18:39:00 +0100

    Finishes equations for simple case

--- crates/cibse_guide_e/src/chapter_6/appendix/a_simple_case.rs
+++ crates/cibse_guide_e/src/chapter_6/appendix/a_simple_case.rs
@@ -14,6 +14,22 @@ pub fn equation_area_of_opening(a_o: String, w_o: String, h_o: String) -> String
     format!("{} = {} \\cdot {}", a_o, w_o, h_o,)
 }
 
+pub fn internal_surface_area(a_f: f64, h: f64, w1: f64, w2: f64, a_o: f64) -> f64 {
+    return 2 * a_f + 2 * h * (w1 + w2) - a_o;
+}
+
+pub fn equation_internal_surface_area(a_net: String, a_f: String, h: String, w1: String, w2: String, a_o: String) -> String {
+    format!("{} = 2 \\cdot {} + 2 \\cdot {} ({} + {}) - {}", a_net, a_f, h, w1, w2, a_o);
+}
+
+pub fn ratio_depth_over_height(w1: f64, w2: f64) -> f64 {
+    return w2 / w1;
+}
+
+pub fn equation_ratio_depth_over_height(d: String, w: String, w1: String, w2: String) -> String {
+    format!("{} / {} = {} / {}", d, w, w2, w1);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,5 +44,17 @@ mod tests {
     fn test_area_of_opening() {
         let result = area_of_opening(0.9, 2.1);
         assert_eq!(result, 1.89);
+    }
+
+    #[test]
+    fn test_internal_surface_area() {
+        let result = internal_surface_area(15.75, 3.0, 4.5, 3.5, 1.89);
+        assert_eq!(result, 43.61);
+    }
+
+    #[test]
+    fn test_ratio_depth_over_height() {
+        let result = ratio_depth_over_height(4.5, 3.5);
+        assert_eq!(result, 0.777777777777778)
     }
 }
