ce80f44a1a88c435a19f3d23a683c6235b78702b
simonsantama <simonsantama@gmail.com>
2025-07-02 21:06:54 +0100

    Finishes implementation of case b, more than one window

--- crates/cibse_guide_e/src/chapter_6/appendix/b_more_than_one_window.rs
+++ crates/cibse_guide_e/src/chapter_6/appendix/b_more_than_one_window.rs
@@ -11,6 +11,25 @@ pub fn equation_equivalent_area_of_openings(a_o: String, areas_of_openings: Vec<
     format!("{} = {}", a_o, formatted_areas)
 }
 
+pub fn equivalent_width_of_openings(widths_of_openings: Vec<f64>) -> f64 {
+    return widths_of_openings.iter().sum();
+}
+
+pub fn equation_equivalent_width_of_openings(w_o: String, widths_of_openings: Vec<String>) -> String {
+    let formatted_widths = widths_of_openings.join(" + ");
+    format!("{} = {}", w_o, formatted_widths)
+}
+
+pub fn equivalent_height_of_openings(equivalent_area_of_openings: f64, equivalent_width_of_openings: f64) -> f64 {
+    if equivalent_width_of_openings == 0.0 {
+        return 0.0; // Avoid division by zero
+    }
+    return equivalent_area_of_openings / equivalent_width_of_openings;
+}
+
+pub fn equation_equivalent_height_of_openings(h_o: String, a_o: String, w_o: String) -> String {
+    format!("{} = {} / {}", h_o, a_o, w_o)
+}
 
 #[cfg(test)]
 mod tests {
@@ -30,4 +49,18 @@ mod tests {
         assert_eq!(result, vec![6.0, 6.0, 2.5]);
     }
 
+    #[test]
+    fn test_equivalent_width_of_openings() {
+        let widths_of_openings = vec![2.0, 3.0, 5.0];
+        let result = equivalent_width_of_openings(widths_of_openings);
+        assert!((result - 10.0).abs() < 1e-10
+    }
+
+    #[test]
+    fn test_equivalent_height_of_openings() {
+        let equivalent_area = 10.0;
+        let equivalent_width = 2.0;
+        let result = equivalent_height_of_openings(equivalent_area, equivalent_width);
+        assert!((result - 5.0).abs() < 1e-10);
+    }
 }
