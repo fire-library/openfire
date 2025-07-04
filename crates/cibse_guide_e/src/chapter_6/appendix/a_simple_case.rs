f604a71636956b26a39cfb5f354c1e642e0a3642
simonsantama <simonsantama@gmail.com>
2025-07-04 18:55:22 +0100

    Fixes errors, adds correct files. Finishes implementation of options a and b

--- crates/cibse_guide_e/src/chapter_6/appendix/a_simple_case.rs
+++ crates/cibse_guide_e/src/chapter_6/appendix/a_simple_case.rs
@@ -1,9 +1,9 @@
 pub fn floor_area(w1: f64, w2: f64) -> f64 {
-    return w1 * w2
+    return w1 * w2;
 }
 
 pub fn floor_area_equation(a_f: String, w1: String, w2: String) -> String {
-    format!("{} = {} \\cdot {}", a_f, w1, w2,)
+    return format!("{} = {} \\cdot {}", a_f, w1, w2,);
 }
 
 pub fn area_of_opening(wo: f64, ho: f64) -> f64 {
@@ -11,23 +11,33 @@ pub fn area_of_opening(wo: f64, ho: f64) -> f64 {
 }
 
 pub fn area_of_opening_equation(a_o: String, w_o: String, h_o: String) -> String {
-    format!("{} = {} \\cdot {}", a_o, w_o, h_o,)
+    return format!("{} = {} \\cdot {}", a_o, w_o, h_o,);
 }
 
 pub fn internal_surface_area(a_f: f64, h: f64, w1: f64, w2: f64, a_o: f64) -> f64 {
-    return 2 * a_f + 2 * h * (w1 + w2) - a_o;
+    return 2.0 * a_f + 2.0 * h * (w1 + w2) - a_o;
 }
 
-pub fn equation_internal_surface_area(a_net: String, a_f: String, h: String, w1: String, w2: String, a_o: String) -> String {
-    format!("{} = 2 \\cdot {} + 2 \\cdot {} ({} + {}) - {}", a_net, a_f, h, w1, w2, a_o);
+pub fn internal_surface_area_equation(
+    a_net: String,
+    a_f: String,
+    h: String,
+    w1: String,
+    w2: String,
+    a_o: String,
+) -> String {
+    return format!(
+        "{} = 2 \\cdot {} + 2 \\cdot {} ({} + {}) - {}",
+        a_net, a_f, h, w1, w2, a_o
+    );
 }
 
 pub fn ratio_depth_over_height(w1: f64, w2: f64) -> f64 {
     return w2 / w1;
 }
 
-pub fn equation_ratio_depth_over_height(d: String, w: String, w1: String, w2: String) -> String {
-    format!("{} / {} = {} / {}", d, w, w2, w1);
+pub fn ratio_depth_over_height_equation(d: String, w: String, w1: String, w2: String) -> String {
+    return format!("{} / {} = {} / {}", d, w, w2, w1);
 }
 
 #[cfg(test)]
@@ -43,18 +53,18 @@ mod tests {
     #[test]
     fn test_area_of_opening() {
         let result = area_of_opening(0.9, 2.1);
-        assert_eq!(result, 1.89);
+        assert_eq!(result, 1.8900000000000001);
     }
 
     #[test]
     fn test_internal_surface_area() {
         let result = internal_surface_area(15.75, 3.0, 4.5, 3.5, 1.89);
-        assert_eq!(result, 43.61);
+        assert_eq!(result, 77.61);
     }
 
     #[test]
     fn test_ratio_depth_over_height() {
         let result = ratio_depth_over_height(4.5, 3.5);
-        assert_eq!(result, 0.777777777777778)
+        assert_eq!(result, 0.7777777777777778)
     }
 }
--- crates/cibse_guide_e/src/chapter_6/appendix/b_more_than_one_window.rs
+++ crates/cibse_guide_e/src/chapter_6/appendix/b_more_than_one_window.rs
@@ -1,12 +1,12 @@
 pub fn areas_of_openings(openings_dimensions: Vec<(f64, f64)>) -> Vec<f64> {
-    return openings_dimensions.iter().map(|(w, h)| w * h).collect()
+    return openings_dimensions.iter().map(|(w, h)| w * h).collect();
 }
 
 pub fn equivalent_area_of_openings(areas_of_openings: Vec<f64>) -> f64 {
-    return areas_of_openings.iter().sum()
+    return areas_of_openings.iter().sum();
 }
 
-pub fn equation_equivalent_area_of_openings(a_o: String, areas_of_openings: Vec<String>) -> String {
+pub fn equivalent_area_of_openings_equation(a_o: String, areas_of_openings: Vec<String>) -> String {
     let formatted_areas = areas_of_openings.join(" + ");
     format!("{} = {}", a_o, formatted_areas)
 }
@@ -15,19 +15,25 @@ pub fn equivalent_width_of_openings(widths_of_openings: Vec<f64>) -> f64 {
     return widths_of_openings.iter().sum();
 }
 
-pub fn equation_equivalent_width_of_openings(w_o: String, widths_of_openings: Vec<String>) -> String {
+pub fn equivalent_width_of_openings_equation(
+    w_o: String,
+    widths_of_openings: Vec<String>,
+) -> String {
     let formatted_widths = widths_of_openings.join(" + ");
     format!("{} = {}", w_o, formatted_widths)
 }
 
-pub fn equivalent_height_of_openings(equivalent_area_of_openings: f64, equivalent_width_of_openings: f64) -> f64 {
+pub fn equivalent_height_of_openings(
+    equivalent_area_of_openings: f64,
+    equivalent_width_of_openings: f64,
+) -> f64 {
     if equivalent_width_of_openings == 0.0 {
         return 0.0; // Avoid division by zero
     }
     return equivalent_area_of_openings / equivalent_width_of_openings;
 }
 
-pub fn equation_equivalent_height_of_openings(h_o: String, a_o: String, w_o: String) -> String {
+pub fn equivalent_height_of_openings_equation(h_o: String, a_o: String, w_o: String) -> String {
     format!("{} = {} / {}", h_o, a_o, w_o)
 }
 
@@ -36,17 +42,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_equivalent_area_of_openings() {
-        let areas_of_openings = vec![1.2, 3.4, 5.6];
-        let result = sum_areas(areas_of_openings);
-        assert!((result - 10.2).abs() < 1e-10);
-    }
-
-    #[test]
     fn test_areas_of_openings() {
         let openings_dimensions = vec![(2.0, 3.0), (1.5, 4.0), (5.0, 0.5)];
         let result = areas_of_openings(openings_dimensions);
         assert_eq!(result, vec![6.0, 6.0, 2.5]);
+    }
+
+    #[test]
+    fn test_equivalent_area_of_openings() {
+        let areas_of_openings = vec![1.2, 3.4, 5.6];
+        let result = equivalent_area_of_openings(areas_of_openings);
+        assert!((result - 10.2).abs() < 1e-10);
     }
 
     #[test]
