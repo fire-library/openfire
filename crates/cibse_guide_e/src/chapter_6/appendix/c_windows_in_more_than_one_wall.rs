674bd8c6d129d2c7270e34fc250474e9e3847b4d
simonsantama <simonsantama@gmail.com>
2025-07-05 10:59:07 +0100

    Finishes implementation of C

--- crates/cibse_guide_e/src/chapter_6/appendix/c_windows_in_more_than_one_wall.rs
+++ crates/cibse_guide_e/src/chapter_6/appendix/c_windows_in_more_than_one_wall.rs
@@ -21,12 +21,12 @@ pub fn sum_area_of_openings_per_wall_equation(
     return common::sum_areas_of_openings_equation(a_o, areas_of_openings);
 }
 
-pub fn total_area_of_openings(areas_of_openings: Vec<f64>) -> f64 {
-    return b_more_than_one_window::total_area_of_openings(areas_of_openings);
+pub fn sum_area_of_openigs(areas_of_openings: Vec<f64>) -> f64 {
+    return common::sum_areas_of_openings(areas_of_openings);
 }
 
-pub fn total_area_of_openings_equation(a_o: String, areas_of_openings: Vec<String>) -> String {
-    return b_more_than_one_window::total_area_of_openings_equation(a_o, areas_of_openings);
+pub fn sum_area_of_openings_equation(a_o: String, areas_of_openings: Vec<String>) -> String {
+    return common::sum_areas_of_openings_equation(a_o, areas_of_openings);
 }
 
 pub fn ratio_depth_over_height(w1: f64, w2: f64, ao_w1: f64, ao: f64) -> f64 {
@@ -50,20 +50,6 @@ pub fn ratio_depth_over_height_equation(
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_total_area_of_openings() {
-        let dimension_openings_wall1 = vec![(2.0, 3.0), (1.5, 4.0), (5.0, 0.5)];
-        let result = total_area_of_openings_per_wall(dimension_openings_wall1);
-        assert_eq!(result, 14.5);
-    }
-
-    #[test]
-    fn test_total_area_of_openings_vec() {
-        let areas = vec![6.0, 6.0, 2.5];
-        let result = total_area_of_openings(areas);
-        assert_eq!(result, 14.5);
-    }
 
     #[test]
     fn test_ratio_depth_over_height() {
