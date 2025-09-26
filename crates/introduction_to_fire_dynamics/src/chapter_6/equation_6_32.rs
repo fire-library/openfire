7a6e800e1bbb059747a44940e7cb146dc31d34c0
simonsantama <simonsantama@gmail.com>
2025-09-26 08:16:02 +0100

    Intermediate commit. Getting off train

---
+++ crates/introduction_to_fire_dynamics/src/chapter_6/equation_6_32.rs
@@ -0,0 +1,22 @@
+use std::f64::consts::PI;
+
+pub fn time_to_ignition_thermally_thick(k: f64, rho: f64, c: f64, temp_ig: f64, temp_o: f64, q_r: f64) -> f64 {
+    return (PI / 4.0)* (k * rho * c) * (temp_ig - temp_o).powf(2.0) / (q_r * 1000.0).powf(2.0);
+
+}
+
+pub fn time_to_ignition_thermallythick_equation(t_ig: String, k: String, rho: String, c: String, temp_ig: String, temp_o: String, q_r: String) -> String {
+    return format!("{} = \\frac{{\\pi}}{{4}} \\cdot {} \\cdot {} \\cdot {} ({} - {})^2 / {}^2", t_ig, k, rho, c, temp_ig, temp_o, q_r);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_time_to_ignition_thermally_thick() {
+        let result = time_to_ignition_thermally_thick(0.19, 1190.0, 1420.0, 300.0, 25.0, 20.0);
+        assert_eq!(result, 47.67428456491);
+    }
+
+}
--- crates/introduction_to_fire_dynamics/src/lib.rs
+++ crates/introduction_to_fire_dynamics/src/lib.rs
@@ -1,1 +1,2 @@
+pub mod chapter_6;
 pub mod chapter_10;
