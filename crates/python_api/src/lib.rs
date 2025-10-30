f5b453b77c5c3e8594efce917ba97fe7e8ec2381
Jamie Maclean <jamie.maclean@numberica.com>
2025-10-30 17:03:00 +0100

    change name of api in python

--- crates/python_api/src/lib.rs
+++ crates/python_api/src/lib.rs
@@ -13,7 +13,7 @@ use pyo3::prelude::*;
 use pyo3::wrap_pymodule;
 
 #[pymodule]
-fn openfire(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn ofire(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_wrapped(wrap_pymodule!(pd_7974::pd_7974))?;
     m.add_wrapped(wrap_pymodule!(br_187::br_187))?;
     m.add_wrapped(wrap_pymodule!(bs9999::bs9999))?;
