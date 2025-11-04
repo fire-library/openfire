bcdc856f1d992b9f2a6c2d441edf903e9e4e6fcc
simonsantama <simonsantama@gmail.com>
2025-11-04 07:08:31 +0000

    Formatting for 2-9

--- crates/fire_dynamics_tools/src/chapter_2/equation_2_9.rs
+++ crates/fire_dynamics_tools/src/chapter_2/equation_2_9.rs
@@ -1,4 +1,10 @@
-pub fn convective_heat_transfer_coefficient(k: f64, rho: f64, c: f64, t: f64, delta: f64) -> f64 {
+pub fn convective_heat_transfer_coefficient(
+    k: f64,
+    rho: f64,
+    c: f64,
+    t: f64,
+    delta: f64,
+) -> f64 {
     let left = (k * rho * c / t).powf(0.5);
     let right = k / delta;
     0.4 * left.max(right)
@@ -29,7 +35,7 @@ mod tests {
         let c = 1.17;
         let t = 180.0;
         let delta = 0.2;
-        let expected_result = 0.07065408693062278;
+        let expected_result = 0.07065408693;
 
         let result = convective_heat_transfer_coefficient(k, rho, c, t, delta);
 
