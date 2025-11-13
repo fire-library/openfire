8108edde63c011b3dad2290ee465aeb9f3e68b87
simonsantama <simonsantama@gmail.com>
2025-11-13 19:29:55 +0000

    Fixes compiling error2

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,11 @@
+pub use br_187;
+pub use bs9999;
+pub use cibse_guide_e;
+pub use fire_dynamics_tools;
+pub use introduction_to_fire_dynamics;
+pub use pd_7974;
+pub use sfpe_handbook;
+pub use tr17;
+
 /// Module for tracking relationships between equations across documents
 pub mod equation_relationships;
