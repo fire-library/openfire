f793332c8e3d86fdc9cf5d2328453e7ea21ef0d0
simonsantama <simonsantama@gmail.com>
2025-12-18 20:21:09 +0000

    Runs cargo fmt

--- crates/eurocode_1_1_2/src/annex_e/equation_e_1.rs
+++ crates/eurocode_1_1_2/src/annex_e/equation_e_1.rs
@@ -1,9 +1,72 @@
-pub fn net_heat_flux_surface(h_net_c: f64, h_net_r: f64) -> f64 {
-    h_net_c + h_net_r
+pub fn design_fire_load(q_f_k: f64, m: f64, delta_q_1: f64, delta_q_2: f64, delta_n: f64) -> f64 {
+    q_f_k * m * delta_q_1 * delta_q_2 * delta_n
 }
 
-pub fn equation(h_net: String, h_net_c: String, h_net_r: String) -> String {
-    format!("{} = {} + {}", h_net, h_net_c, h_net_r)
+pub fn design_fire_load_equation(
+    q_f_d: String,
+    q_f_k: String,
+    m: String,
+    delta_q_1: String,
+    delta_q_2: String,
+    delta_n: String,
+) -> String {
+    format!(
+        "{} = {} * {} * {} * {} * {}",
+        q_f_d, q_f_k, m, delta_q_1, delta_q_2, delta_n
+    )
+}
+
+pub fn delta_n(
+    delta_n_1: f64,
+    delta_n_2: f64,
+    delta_n_3: f64,
+    delta_n_4: f64,
+    delta_n_5: f64,
+    delta_n_6q: f64,
+    delta_n_7: f64,
+    delta_n_8: f64,
+    delta_n_9: f64,
+    delta_n_10: f64,
+) -> f64 {
+    delta_n_1
+        * delta_n_2
+        * delta_n_3
+        * delta_n_4
+        * delta_n_5
+        * delta_n_6q
+        * delta_n_7
+        * delta_n_8
+        * delta_n_9
+        * delta_n_10
+}
+
+pub fn delta_n_equation(
+    delta_n: String,
+    delta_n_1: String,
+    delta_n_2: String,
+    delta_n_3: String,
+    delta_n_4: String,
+    delta_n_5: String,
+    delta_n_6: String,
+    delta_n_7: String,
+    delta_n_8: String,
+    delta_n_9: String,
+    delta_n_10: String,
+) -> String {
+    format!(
+        "{} = {} * {} * {} * {} * {} * {} * {} * {} * {} * {}",
+        delta_n,
+        delta_n_1,
+        delta_n_2,
+        delta_n_3,
+        delta_n_4,
+        delta_n_5,
+        delta_n_6,
+        delta_n_7,
+        delta_n_8,
+        delta_n_9,
+        delta_n_10
+    )
 }
 
 #[cfg(test)]
@@ -11,8 +74,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_net_heat_flux_surface() {
-        let result = net_heat_flux_surface(15000.0, 25000.0);
-        assert_eq!(result, 40000.0);
+    fn test_design_fire_load() {
+        let delta_n_variable = delta_n(0.61,
+            1.0, 0.87, 0.87,
+            0.87, 0.61, 0.61,
+            0.9, 1.0, 1.0);
+        let result = design_fire_load(780.0, 0.8, 1.1, 0.78, delta_n_variable);
+        let expected = 135.7102224;
+        assert_eq!(result, expected);
+        // assert!((result - expected).abs() < 1e-6);
     }
 }
