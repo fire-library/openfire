03bb7705e640ab3293a7afad7bb1a883fdbd1526
simonsantama <simonsantama@gmail.com>
2025-12-20 13:41:33 +0000

    Finishes the rust implementation

--- crates/sfpe_handbook/src/chapter_50/equation_50_1.rs
+++ crates/sfpe_handbook/src/chapter_50/equation_50_1.rs
@@ -1,11 +1,11 @@
-pub fn delta_p_so(t_0: f64, t_s: f64) -> f64 {
+pub fn pressure_difference_stack_effect(t_0: f64, t_s: f64, z: f64) -> f64 {
     let t_0_abs = t_0 + 273.0;
     let t_s_abs = t_s + 273.0;
-    3460.0 * (1.0 / (t_0_abs) + 1.0 / (t_s_abs)) 
+    3460.0 * (1.0 / (t_0_abs) - 1.0 / (t_s_abs)) * z 
 }
 
-pub fn delta_p_so_equation(delta_p_so: String, t_0: String, t_s: String) -> String {
-    format!("{} = 3460 * (1 / ({} + 273) + 1 / ({} + 273))", delta_p_so, t_0, t_s)
+pub fn pressure_difference_stack_effect_equation(delta_p_so: String, t_0: String, t_s: String, z: String) -> String {
+    format!("{} = 3460 * (1 / ({} + 273) - 1 / ({} + 273)) * {}", delta_p_so, t_0, t_s, z)
 }
 
 #[cfg(test)]
@@ -13,9 +13,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_delta_p_so() {
-        let result = delta_p_so(27.0, 47.0);
-        let expected = 0.0;
+    fn test_pressure_difference_stack_effect() {
+        let result = pressure_difference_stack_effect(-18.0, 21.0, 30.0);
+        let expected = 53.99759904;
         assert!((result - expected).abs() < 1e-6);
     }
 }
