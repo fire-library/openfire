5b7d6e9424040f404892aa925fd474e0999aaf03
Simon Santamaria <simonsantama@gmail.com>
2025-12-21 09:16:43 +0000

    Update crates/python_api/src/sfpe_handbook/chapter_50/equation_50_2.rs
    Co-authored-by: Jamie Maclean <38719252+JamieMaclean@users.noreply.github.com>

--- crates/python_api/src/sfpe_handbook/chapter_50/equation_50_2.rs
+++ crates/python_api/src/sfpe_handbook/chapter_50/equation_50_2.rs
@@ -38,7 +38,7 @@ use openfire::sfpe_handbook::chapter_50::equation_50_2 as rust_equation_50_2;
 ///     >>> import ofire
 ///     >>> result = ofire.sfpe_handbook.chapter_50.equation_50_2.pressure_difference_fire_compartment(20.0, 800.0, 1.52)
 ///     >>> print(f"{result:.2f} Pa")
-fn pressure_difference_fire_compartment(t_0: f64, t_f: f64, z: f64) -> PyResult<f64> {
+fn pressure_difference(t_0: f64, t_f: f64, z: f64) -> PyResult<f64> {
     Ok(rust_equation_50_2::pressure_difference_fire_compartment(
         t_0, t_f, z,
     ))
